cc34cf60e716b33b5a5f3555c34c53e8aebcbf83
Stiwi Gabriel Courage <stiwi.courage@twoday.com>
2025-03-10 22:09:52 +0100

    First commit (#2)
    Co-authored-by: Stiwi Gabriel Courage <stiwi.courage@opensquare.dk>

--- src/main/java/dk/digst/oots/edelivery/client/Main.java
+++ src/main/java/dk/digst/oots/edelivery/client/Main.java
@@ -3,5 +3,6 @@ package dk.digst.oots.edelivery.client;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Nice to finally meet you");
     }
 }
