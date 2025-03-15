287380fdf0aaf57f55a754b54d79e40dc6be8601
Stiwi Gabriel Courage <stiwi.courage@twoday.com>
2025-03-15 17:55:01 +0100

    Setting up test with Spock.

---
+++ src/test/groovy/dk/digst/oots/edelivery/client/StandardSpecification.groovy
@@ -0,0 +1,10 @@
+package dk.digst.oots.edelivery.client
+
+import spock.lang.Specification
+
+class StandardSpecification extends Specification {
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+}
